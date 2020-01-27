8c845b1e7f21a6a345e25d2ff82ba67ca07a7b39
Nicolas Raymundo <nicoraymundo2@gmail.com>
2020-01-27 12:33:59 -0800

    Edited package.json and readme

---
+++ build/precache-manifest.9617d6aef7257e755fe7de94a95cef88.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d5465f8517ad45a2406eb9ef25e5fe4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7ae5bc4dd1d2cd6532a",
+    "url": "/static/css/main.4c77e541.chunk.css"
+  },
+  {
+    "revision": "ee035aa4e8c7cb4d5f9e",
+    "url": "/static/js/2.5296ffeb.chunk.js"
+  },
+  {
+    "revision": "f7ae5bc4dd1d2cd6532a",
+    "url": "/static/js/main.3e2d0736.chunk.js"
+  },
+  {
+    "revision": "bce8287e88757295a62c",
+    "url": "/static/js/runtime-main.57751864.js"
+  },
+  {
+    "revision": "518eb909ff651bb44d29e0da3f921ba5",
+    "url": "/static/media/email.518eb909.jpg"
+  },
+  {
+    "revision": "c3c5a6e3d33fd10ebdd04ef86c48ccd7",
+    "url": "/static/media/insta.c3c5a6e3.png"
+  },
+  {
+    "revision": "cb938f07debd620d4acbcddc4070eb8a",
+    "url": "/static/media/resume.cb938f07.pdf"
+  }
+]);

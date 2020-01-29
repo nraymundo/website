897eb3f3a15e4988dc7e28ec58700255613c873c
Nicolas Raymundo <nicoraymundo2@gmail.com>
2020-01-28 16:37:20 -0800

    update website

---
+++ build/precache-manifest.7b6f77fdc0af9a25c87ea688721ddc3f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8484185f2cc575e589ca871a0accd0cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02ad37b94999dbc234ca",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "84ea8c76c70c4ff6d0c8",
+    "url": "/static/css/main.49da3e6b.chunk.css"
+  },
+  {
+    "revision": "02ad37b94999dbc234ca",
+    "url": "/static/js/2.48ea38d8.chunk.js"
+  },
+  {
+    "revision": "84ea8c76c70c4ff6d0c8",
+    "url": "/static/js/main.6da4baf6.chunk.js"
+  },
+  {
+    "revision": "143ce4b75225907fcd20",
+    "url": "/static/js/runtime-main.794c657b.js"
+  },
+  {
+    "revision": "518eb909ff651bb44d29e0da3f921ba5",
+    "url": "/static/media/email.518eb909.jpg"
+  },
+  {
+    "revision": "c3c5a6e3d33fd10ebdd04ef86c48ccd7",
+    "url": "/static/media/insta.c3c5a6e3.png"
+  },
+  {
+    "revision": "cb938f07debd620d4acbcddc4070eb8a",
+    "url": "/static/media/resume.cb938f07.pdf"
+  }
+]);

5425ea9832e42e9ae5a0e61ae83f951fbc042c86
Nicolas Raymundo <nicoraymundo2@gmail.com>
2020-03-31 18:47:57 -0700

    Add about me

---
+++ src/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Fade from "react-reveal/Fade";
+
+import "./About.css";
+
+const About = () => {
+  return (
+    <div className="about-container">
+      <div className="about-me-container">
+        <h1 className="about-title">About Me</h1>
+        <hr className="horizontal-line" />
+      </div>
+    </div>
+  );
+};
+
+export default About;

57322aa32d09af8de9a9c232cfa2f188713aa567
Nicolas Raymundo <nicoraymundo2@gmail.com>
2020-04-01 22:03:29 -0700

    Update link decoration

--- src/Projects.js
+++ src/Projects.js
@@ -17,46 +17,45 @@ const Projects = () => {
       </div>
       <div className="project-individual-container-top">
         <img src={playroll2} alt="playroll" className="playroll-img" />
-        <h1 className="project-individual-title">Playroll</h1>
+        <a href="" target="_blank" className="project-individual-title">
+          Playroll
+        </a>
         <h1 className="project-individual-date">Spring 2019</h1>
         <hr className="horizontal-line-projects" />
       </div>
       <div className="project-individual-container">
         <img src={pivot} alt="pivot" className="pivot-img" />
-        <h1
-          onClick={() =>
-            window.open("https://github.com/wdibi/Pivot", "_blank")
-          }
+        <a
+          href="https://github.com/wdibi/Pivot"
           className="project-individual-title"
+          target="_blank"
         >
           Pivot
-        </h1>
+        </a>
         <h1 className="project-individual-date">Spring 2020</h1>
         <hr className="horizontal-line-projects" />
       </div>
       <div className="project-individual-container">
         <img src={soccer} alt="soccer" className="pivot-img" />
-        <h1
-          onClick={() =>
-            window.open("https://github.com/nraymundo/cmsi370", "_blank")
-          }
+        <a
+          href="https://github.com/nraymundo/cmsi370"
           className="project-individual-title"
+          target="_blank"
         >
           Take That For Data
-        </h1>
+        </a>
         <h1 className="project-individual-date">Fall 2019</h1>
         <hr className="horizontal-line-projects" />
       </div>
       <div className="project-individual-container-bottom">
         <img src={jonah} alt="jonah" className="jonah-img" />
-        <h1
-          onClick={() =>
-            window.open("https://github.com/nraymundo/cmsi385", "_blank")
-          }
+        <a
+          href="https://github.com/nraymundo/cmsi385"
           className="project-individual-title"
+          target="_blank"
         >
           NFA Simulator
-        </h1>
+        </a>
         <h1 className="project-individual-date">Fall 2019</h1>
         <hr className="horizontal-line-projects" />
       </div>

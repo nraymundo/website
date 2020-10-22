2014b658f877547791a5853e390cdc2c2f37ce60
Nicolas Raymundo <nicoraymundo2@gmail.com>
2020-10-21 22:47:03 -0700

    Add project overlay

--- src/Projects.js
+++ src/Projects.js
@@ -7,6 +7,7 @@ import pivot from "./images/Projects/pivot.jpg";
 import nfa from "./images/Projects/nfa.jpg";
 import playroll from "./images/Projects/playroll.jpg";
 import soccer from "./images/Projects/soccer.jpg";
+import github from "./images/Contact/github.png";
 
 const Projects = () => {
   return (
@@ -17,16 +18,25 @@ const Projects = () => {
       </div>
       <div className="projects-container">
         <div className="project-individual-container-top">
-          {/* <img src={playroll2} alt="playroll" /> */}
-          <span className="overlay">
-            <a
-              href="https://github.com/wdibi/Pivot"
-              target="_blank"
-              className="clickable-img"
-            >
-              <img src={pivot} alt="pivot" className="pivot-img" />
-            </a>
-          </span>
+          <div className="container">
+            <img src={pivot} alt="pivot" className="project-img" />
+            <div className="middle">
+              <div className="text">
+                <h1>Pivot</h1>
+                <p>
+                  A custom programming language using a custom compiler with
+                  semantic analysis, code generation, and code optimization.
+                </p>
+                <a
+                  href="https://github.com/wdibi/Pivot"
+                  target="_blank"
+                  className="repo-container"
+                >
+                  <img src={github} alt="pivot-repo" className="repo"></img>
+                </a>
+              </div>
+            </div>
+          </div>
           <a
             href="https://github.com/wdibi/Pivot"
             target="_blank"
@@ -38,15 +48,25 @@ const Projects = () => {
         </div>
         <hr className="horizontal-line-projects" />
         <div className="project-individual-container-top">
-          <span className="overlay">
-            <a
-              href="https://github.com/wdibi/Pivot"
-              target="_blank"
-              className="clickable-img"
-            >
-              <img src={nfa} alt="nfa" className="jonah-img" />
-            </a>
-          </span>
+          <div className="container">
+            <img src={nfa} alt="nfa" className="project-img" />
+            <div className="middle">
+              <div className="text">
+                <h1>NFA Simulator</h1>
+                <p>
+                  A nondeterministic finite automata simulator built with
+                  Python.
+                </p>
+                <a
+                  href="https://github.com/nraymundo/cmsi385"
+                  target="_blank"
+                  className="repo-container"
+                >
+                  <img src={github} alt="pivot-repo" className="repo"></img>
+                </a>
+              </div>
+            </div>
+          </div>
           <a
             href="https://github.com/nraymundo/cmsi385"
             target="_blank"
@@ -55,7 +75,6 @@ const Projects = () => {
             NFA Simulator
           </a>
           <h1 className="project-individual-date">Fall 2019</h1>
-          {/* <hr className="horizontal-line-projects" /> */}
         </div>
       </div>
     </div>

47e46b49dab274727b283deb34c0f13b87f22b6e
Nicolas Raymundo <nicoraymundo2@gmail.com>
2020-10-21 18:35:41 -0700

    Update projects

--- src/Projects.js
+++ src/Projects.js
@@ -3,7 +3,8 @@ import React from "react";
 import "./Projects.css";
 
 import jonah from "./images/Filler/jonah.jpg";
-// import pivot from "./images/Projects/pivot.jpg";
+import pivot from "./images/Projects/pivot.jpg";
+import nfa from "./images/Projects/nfa.jpg";
 import playroll from "./images/Projects/playroll.jpg";
 import playroll2 from "./images/Projects/loading.png";
 import soccer from "./images/Projects/soccer.jpg";
@@ -18,76 +19,43 @@ const Projects = () => {
       <div className="projects-container">
         <div className="project-individual-container-top">
           {/* <img src={playroll2} alt="playroll" /> */}
-          <img src={playroll2} alt="playroll" className="playroll-img" />
-          <a href="" target="_blank" className="project-individual-title">
+          <a
+            href="https://github.com/wdibi/Pivot"
+            target="_blank"
+            className="clickable-img"
+          >
+            <img src={pivot} alt="pivot" className="pivot-img" />
+          </a>
+          <a
+            href="https://github.com/wdibi/Pivot"
+            target="_blank"
+            className="project-individual-title"
+          >
             Pivot
           </a>
           <h1 className="project-individual-date">Spring 2020</h1>
-          {/* <hr className="horizontal-line-projects" /> */}
         </div>
         <hr className="horizontal-line-projects" />
         <div className="project-individual-container-top">
-          {/* <img src={playroll2} alt="playroll" /> */}
-          <img src={jonah} alt="jonah" className="jonah-img" />
-          <a href="" target="_blank" className="project-individual-title">
+          <a
+            href="https://github.com/wdibi/Pivot"
+            target="_blank"
+            className="clickable-img"
+          >
+            <img src={nfa} alt="nfa" className="jonah-img" />
+          </a>
+          <a
+            href="https://github.com/nraymundo/cmsi385"
+            target="_blank"
+            className="project-individual-title"
+          >
             NFA Simulator
           </a>
           <h1 className="project-individual-date">Fall 2019</h1>
           {/* <hr className="horizontal-line-projects" /> */}
         </div>
-        <hr className="horizontal-line-projects" />
       </div>
-      {/* <div className="project-individual-container">
-        <img src={jonah} alt="jonah" className="jonah-img" />
-        <a
-          href="https://github.com/wdibi/Pivot"
-          className="project-individual-title"
-          target="_blank"
-        >
-          Pivot
-        </a>
-        <h1 className="project-individual-date">Spring 2020</h1>
-        <hr className="horizontal-line-projects" />
-      </div>
-      <div className="project-individual-container">
-        <img src={soccer} alt="soccer" className="pivot-img" />
-        <a
-          href="https://github.com/nraymundo/cmsi370"
-          className="project-individual-title"
-          target="_blank"
-        >
-          Take That For Data
-        </a>
-        <h1 className="project-individual-date">Fall 2019</h1>
-        <hr className="horizontal-line-projects" />
-      </div>
-      <div className="project-individual-container-bottom">
-        <img src={jonah} alt="jonah" className="jonah-img" />
-        <a
-          href="https://github.com/nraymundo/cmsi385"
-          className="project-individual-title"
-          target="_blank"
-        >
-          NFA Simulator
-        </a>
-        <h1 className="project-individual-date">Fall 2019</h1>
-        <hr className="horizontal-line-projects" />
-      </div> */}
     </div>
-
-    // <div className="MainContainer">
-    //   <div className="Header-Projects">
-    //     <h1>Projects</h1>
-    //   </div>
-    //   <div className="Menu-Projects">
-    //     <div className="Container-Projects">
-    //       <img src={pivot} alt="pivot" className="Logo"></img>
-    //       <div class="Info-Projects">
-    //         <div class="Info-Name">Pivot</div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
